7d6cf84ec5512c6080878f073cd3db9d3b57650a
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-07-09 20:25:07 +0500

    first commit

---
+++ twitterClone/twitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterCloneApp.swift
+//  twitterClone
+//
+//  Created by Dias Yerlan on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct twitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
